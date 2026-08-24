f664960fa3ea3b152ad8b277ea30112f72018e64
peknight <JKpeknight@gmail.com>
2026-08-24 17:21:57 +0800

    refactor(socks5): Socks5PullState 继承 BytePullStateErrorDsl 精简样板
    利用 fs2 新增的 BytePullStateErrorDsl，把 lift*T 无参重载、pull/parse
    /readSized* 的 eof: => Throwable 重载、无参 attempt 全部上提；Socks5PullState
    只需实现 error(state, throwable) = state.error(throwable)，外加 setS 终止态
    校验、typed/typedS 的 WrongClassTag 默认错误和 outputS 特有扩展。
    
    测试 Socks5ServerFlatSpec 的常驻 serve 流加 interruptAfter(10.seconds)，
    使用例能在 10 秒后正常结束而非挂起。

--- socks/socks5/server/core/src/main/scala/com/peknight/socks5/server/state/Socks5PullState.scala
+++ socks/socks5/server/core/src/main/scala/com/peknight/socks5/server/state/Socks5PullState.scala
@@ -1,93 +1,30 @@
 package com.peknight.socks5.server.state
 
 import com.peknight.error.std.WrongClassTag
-import com.peknight.fs2.pull.state.BytePullStateDsl
+import com.peknight.fs2.pull.state.BytePullStateErrorDsl
 import com.peknight.socks5.server.state.State.Terminated
-import fs2.Stream.ToPull
-import fs2.{Chunk, Pull, Stream}
 import scodec.bits.ByteVector
 
-import java.nio.charset.Charset
 import scala.reflect.ClassTag
 
 /**
- * SOCKS5 服务端状态机专用的 [[BytePullStateDsl]]：固定 `S = State`、`E = Terminated`，
- * 并把 `Throwable` 统一通过 `State.error` 提升为 `Terminated`，EOF 直接接收 `=> Throwable`。
+ * SOCKS5 服务端状态机专用的 [[BytePullStateErrorDsl]]：
+ * 固定 `S = State`、`E = Terminated`，底层 `Throwable` 统一通过 `State.error` 提升。
  */
-object Socks5PullState extends BytePullStateDsl[State, Terminated]:
+object Socks5PullState extends BytePullStateErrorDsl[State, Terminated]:
+
+  def error(state: State, throwable: Throwable): Terminated = state.error(throwable)
 
   override def setS[F[_]](s: State): PS[F, Unit] =
     s match
       case terminated: Terminated => liftL(terminated)
       case state => super.setS(state)
 
-  def liftPET[F[_], A](pull: Pull[F, Byte, Either[Throwable, A]]): PS[F, A] =
-    super.liftPET(pull)(_.error(_))
-  def liftPLT[F[_], A](pull: Pull[F, Byte, Throwable]): PS[F, A] =
-    super.liftPLT(pull)(_.error(_))
-  def liftFET[F[_], A](f: F[Either[Throwable, A]]): PS[F, A] =
-    super.liftFET(f)(_.error(_))
-  def liftFLT[F[_], A](f: F[Throwable]): PS[F, A] =
-    super.liftFLT(f)(_.error(_))
-  def liftET[F[_], A](either: Either[Throwable, A]): PS[F, A] =
-    super.liftET(either)(_.error(_))
-  def liftT[F[_], A](t: Throwable): PS[F, A] =
-    super.liftT(t)(_.error(_))
-
-  def pull[F[_], A](f: ToPull[F, Byte] => Pull[F, Byte, Option[(A, Stream[F, Byte])]])(eof: => Throwable)
-  : PS[F, A] =
-    super.pull(f)(_.error(eof))
-
-  def map[F[_], I, A](f: ToPull[F, Byte] => Pull[F, Byte, Option[(I, Stream[F, Byte])]])
-                     (g: I => A)(eof: => Throwable): PS[F, A] =
-    super.map(f)(g)(_.error(eof))
-
-  def map1[F[_], A](f: Byte => A)(eof: => Throwable): PS[F, A] =
-    super.map1(f)(_.error(eof))
-
-  def mapChunk[F[_], A](f: ToPull[F, Byte] => Pull[F, Byte, Option[(Chunk[Byte], Stream[F, Byte])]])
-                       (g: Chunk[Byte] => A)(eof: => Throwable): PS[F, A] =
-    super.mapChunk(f)(g)(_.error(eof))
-
-  def parse[F[_], I, A](f: ToPull[F, Byte] => Pull[F, Byte, Option[(I, Stream[F, Byte])]])
-                       (g: I => Either[Throwable, A])(eof: => Throwable): PS[F, A] =
-    super.parse(f)(g)(_.error(_))(_.error(eof))
-
-  def parse1[F[_], A](f: Byte => Either[Throwable, A])(eof: => Throwable): PS[F, A] =
-    super.parse1(f)(_.error(_))(_.error(eof))
-
-  def parseChunk[F[_], A](f: ToPull[F, Byte] => Pull[F, Byte, Option[(Chunk[Byte], Stream[F, Byte])]])
-                         (g: Chunk[Byte] => Either[Throwable, A])(eof: => Throwable): PS[F, A] =
-    super.parseChunk(f)(g)(_.error(_))(_.error(eof))
-
-  def readSizedBytes[F[_]](eof: => Throwable): PS[F, Chunk[Byte]] =
-    super.readSizedBytes(_.error(eof))
-
-  def mapSizedBytes[F[_], A](f: Chunk[Byte] => A)(eof: => Throwable): PS[F, A] =
-    super.mapSizedBytes(f)(_.error(eof))
-
-  def parseSizedBytes[F[_], A](f: Chunk[Byte] => Either[Throwable, A])(eof: => Throwable): PS[F, A] =
-    super.parseSizedBytes(f)(_.error(_))(_.error(eof))
-
-  def readSizedString[F[_]](eof: => Throwable)(using Charset): PS[F, String] =
-    super.readSizedString(_.error(_))(_.error(eof))
-
-  def mapSizedString[F[_], A](f: String => A)(eof: => Throwable)(using Charset): PS[F, A] =
-    super.mapSizedString(f)(_.error(_))(_.error(eof))
-
-  def parseSizedString[F[_], A](f: String => Either[Throwable, A])(eof: => Throwable)(using Charset): PS[F, A] =
-    super.parseSizedString(f)(_.error(_))(_.error(eof))
-
   def typed[F[_], A: ClassTag](any: Any): PS[F, A] =
     super.typed[F, Any, A](any)((s, a) => s.error(WrongClassTag[A](a)))
 
   def typedS[F[_], A: ClassTag]: PS[F, A] =
     super.typedS(s => s.error(WrongClassTag[A](s)))
-
-  extension [F[_], A](state: PS[F, A])
-    /** 无参版本：把底层 throwable 通过当前 State 提升为 Terminated。 */
-    def attempt: PS[F, A] = super[BytePullStateDsl].attempt(state)(_.error(_))
-  end extension
 
   extension [F[_]](state: PS[F, State])
     def outputS(f: State => ByteVector): PS[F, State] =
--- socks/socks5/server/io/src/test/scala/com/peknight/socks5/server/io/Socks5ServerFlatSpec.scala
+++ socks/socks5/server/io/src/test/scala/com/peknight/socks5/server/io/Socks5ServerFlatSpec.scala
@@ -8,6 +8,8 @@ import com.peknight.socks5.server.io.api.DirectConnectApi
 import fs2.{Pipe, Stream}
 import org.scalatest.flatspec.AsyncFlatSpec
 
+import scala.concurrent.duration.*
+
 class Socks5ServerFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
   "Socks5 Server" should "pass" in {
     val socks5ServerApi = Socks5ServerApi[IO, Unit, Resource[IO, (Pipe[IO, Byte, Unit], Stream[IO, Byte])], Unit, Unit](
@@ -21,6 +23,7 @@ class Socks5ServerFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
       UDPAssociateApi.unsupported[IO, Unit]
     )
     val socks5Server = Socks5Server(socks5ServerApi, SocketAddress.port(port"1088"))
-    socks5Server.serve.compile.drain.asserting(_ => assert(true))
+    // serve 是常驻流，测试只验证其能正常拉起，10 秒后中断以让用例结束
+    socks5Server.serve.interruptAfter(10.seconds).compile.drain.asserting(_ => assert(true))
   }
 end Socks5ServerFlatSpec
